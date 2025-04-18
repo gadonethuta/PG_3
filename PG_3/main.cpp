0aed9c6b31d0e84fdbeaebf5fcea6438be344a70
gadonethuta <k024g1035@g.neec.ac.jp>
2025-04-18 14:27:52 +0900

    0.2

--- PG_3/main.cpp
+++ PG_3/main.cpp
@@ -3,11 +3,14 @@
 #include <time.h>
 #include <Windows.h>
 
+typedef void (*newType)(int, int);
+
 
 
 int main() {
 	//ランダムのたねを作る
 	srand(time(nullptr));
+	newType CompareResult;
 
 	//エラーならー１のため初期化
 	int diceRandom = -1;
@@ -16,6 +19,15 @@ int main() {
 	diceRandom = rand() % 6 + 1;
 
 	printf("random = %d\n", diceRandom);
+	
+	printf("0偶数の場合は0、奇数の場合は1を押してください : ");
+	scanf_s("%d", &playerInput);
+
+	if (diceRandom % 2 == playerInput) {
+		printf("Correct");
+	} else {
+		printf("False");
+	}
 
 	return 0;
 }
