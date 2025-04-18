a314c7cf60c6759d0fed4e4ffd9d1856778a1870
gadonethuta <k024g1035@g.neec.ac.jp>
2025-04-18 14:19:47 +0900

    0.1

--- PG_3/main.cpp
+++ PG_3/main.cpp
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <Windows.h>
 
+
+
 int main() {
-	
+	//ランダムのたねを作る
+	srand(time(nullptr));
+
+	int diceRandom = -1;//エラーならー１のため初期化
+	diceRandom = rand() % 6 + 1;
+
+	printf("%d/n", diceRandom);
+
 	return 0;
 }
