777efaa486fb178726092fab050b449ad4f97f2e
gadonethuta <k024g1035@g.neec.ac.jp>
2025-04-18 14:45:25 +0900

    ver1

--- PG_3/main.cpp
+++ PG_3/main.cpp
@@ -3,35 +3,64 @@
 #include <time.h>
 #include <Windows.h>
 
-typedef void (*newType)(int, int);
+//関数ポインター
+typedef void (*newType)(int *, int *);
 
-void SetTimeout(newType func, int second);
+/// <summary>
+/// 時間少し立ってから関数を呼ぶ
+/// </summary>
+/// <param name="func">関数ポインター</param>
+/// <param name="a">変数１</param>
+/// <param name="b">変数２</param>
+void SetTimeout(newType func, int a, int b);
+
+/// <summary>
+/// サイコロの結果とプレイヤーの答えを比べる
+/// </summary>
+/// <param name="diceResult">サイコロの結果</param>
+/// <param name="playerGuess">プレイヤーの答え</param>
+void Compare(int *diceResult, int *playerGuess);
 
 int main() {
 	//ランダムのたねを作る
-	srand(time(nullptr));
-	newType CompareResult;
+	srand(static_cast<int>(time(nullptr)));
+
+	//関数ポインターの初期化
+	newType pFunc;
+	pFunc = Compare;
 
 	//エラーならー１のため初期化
 	int diceRandom = -1;
 	int playerInput = -1;
 
+	//サイコロをランダムする
 	diceRandom = rand() % 6 + 1;
 
-	printf("random = %d\n", diceRandom);
+	//試してみるため
+	//printf("random = %d\n", diceRandom);
 	
-	printf("0偶数の場合は0、奇数の場合は1を押してください : ");
+	//入力
+	printf("偶数の場合は0、奇数の場合は1を押してください : ");
 	scanf_s("%d", &playerInput);
 
-	if (diceRandom % 2 == playerInput) {
-		printf("Correct");
-	} else {
-		printf("False");
-	}
+	//時間立ってから関数ポインターを呼ぶ
+	SetTimeout(pFunc, diceRandom, playerInput);
 
 	return 0;
 }
 
-void SetTimeout(newType func, int second) {
-	
+void SetTimeout(newType func, int a, int b) {
+	//3秒止まる
+	Sleep(3000);
+	//関数ポインターを起動
+	func(&a, &b);
+}
+
+void Compare(int *diceResult, int *playerGuess) {
+	//サイコロの結果とプレイヤーの答えを比べる
+	if (*diceResult % 2 == *playerGuess) {
+		printf("正解\n");
+	} else {
+		printf("不正解, サイコロは%dでした。\n", *diceResult);
+	}
 }
