3454b972e2440f91c1c244bdc11b5536d4ebaeb7
gadonethuta <k024g1035@g.neec.ac.jp>
2025-04-18 14:04:14 +0900

    test2

--- PG_3/main.cpp
+++ PG_3/main.cpp
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
 void PrintHelloWorld() {
-	printf("Hello World!");
+	printf("Hello World!\n");
+}
+
+int Add(int a, int b) {
+	return a + b;
+}
+
+int Subtract(int a, int b) {
+	return a - b;
 }
 
 int main() {
@@ -10,5 +18,16 @@ int main() {
 	printf("printHelloWorldのアドレス＝%p\n",PrintHelloWorld);
 	printf("pFuncの内容＝%p\n", *pFunc);
 	pFunc();//ここで本当に起動
+
+	int l = 114;
+	int h = 514;
+	int (*calc)(int, int);
+
+	calc = Add;
+	printf("calc = %d\n", calc(l,h));
+
+	calc = Subtract;
+	printf("calc = %d\n", calc(l, h));
+
 	return 0;
 }
