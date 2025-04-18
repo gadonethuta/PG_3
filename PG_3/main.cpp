3a42a4921682128e610561638f022bec69c35b1a
gadonethuta <k024g1035@g.neec.ac.jp>
2025-04-18 13:55:15 +0900

    asd

--- PG_3/main.cpp
+++ PG_3/main.cpp
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+void PrintHelloWorld() {
+	printf("Hello World!");
+}
+
 int main() {
-	printf("main = %p\n", main);
+	void (*pFunc)();
+	pFunc = PrintHelloWorld;
+	printf("printHelloWorldのアドレス＝%p\n",PrintHelloWorld);
+	printf("pFuncの内容＝%p\n", *pFunc);
+	pFunc();//ここで本当に起動
 	return 0;
 }
