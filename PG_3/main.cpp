fa98b5f6101ca3c4a6816cad22c2346e800b7153
gadonethuta <k024g1035@g.neec.ac.jp>
2025-05-09 14:09:54 +0900

    tested

--- PG_3/main.cpp
+++ PG_3/main.cpp
@@ -1,8 +1,20 @@
 #include <stdio.h>
+//3
+#include<functional>
 
 int main(int argc, const char *argv[]) {
+	//1
+	//[]() {printf("lamda test"); }();
 	
-	[]() {printf("lamda test"); }();
+	//2
+	/*int num = 10;
+	auto fx = [=](int i) {return num + i; };
 
+	printf("%d", fx(2));*/
+	
+	//3
+	std::function<int(int)> fx = [](int i) {return i + 1; };
+	auto fx2 = [](int i) { return i + 1; };
+	printf("%d\n%d\n", fx(2), fx2(5));
 	return 0;
 }
