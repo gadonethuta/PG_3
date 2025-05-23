b5f4786181945f52b384e4601f8d7f142c13c749
gadonethuta <k024g1035@g.neec.ac.jp>
2025-05-23 14:11:16 +0900

    test1

--- PG_3/main.cpp
+++ PG_3/main.cpp
@@ -3,15 +3,40 @@
 #include <time.h>
 #include <Windows.h>
 
-//敵クラス入れる
-#include "Enemy.h"
+//-----------クラステンプレート
+//template <typename T>
+//class Box {
+//public:
+//	T height_;
+//	T width_;
+//
+//	Box(T height, T width) : height_(height), width_(width) {}
+//
+//	T Size() {
+//		return height_ * width_;
+//	}
+//};
+//
+//int main() {
+//	
+//	Box<int> b1(10, 3);
+//	Box<double> b2(12.0, 2.0);
+//
+//	printf("%d\n", b1.Size());
+//	printf("%lf", b2.Size());
+//	
+//	return 0;
+//}
 
-
-int main() {
-	//敵のインスタンスを作る
-	Enemy *enemy_ = new Enemy();
-	//敵の処理
-	enemy_->Update();
-	return 0;
+//--------------関数テンプレート
+template <typename Type>
+Type Add(Type a, Type b) {
+	return static_cast<Type>(a + b);
 }
 
+int main() {
+	printf("%d\n", Add<int>(10, 2));
+	printf("%f\n", Add<float>(11.4f, 51.4f));
+
+	return 0;
+}
