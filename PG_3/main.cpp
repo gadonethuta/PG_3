b2bf0cf1464172b9ca071b15263e077fe1ef04b7
gadonethuta <k024g1035@g.neec.ac.jp>
2025-05-23 16:27:16 +0900

    reset

--- PG_3/main.cpp
+++ PG_3/main.cpp
@@ -3,34 +3,8 @@
 #include <time.h>
 #include <Windows.h>
 
-#include "Sword.h"
-#include "Gun.h"
-
 int main() {
-	//Weaponクラスでインスタンス３つ作る
-	Weapon* weapon[3];
-
-	//GunとSwordをインスタンス化する
-	for (int i = 0; i < 2; i++) {
-		if (i == 0) {
-			weapon[i] = new Gun();
-		} else {
-			weapon[i] = new Sword();
-		}
-	}
-
-	//ポリモーフィズムを使って攻撃する
-	for (int i = 0; i < 2; i++) {
-		weapon[i]->Attack();
-	}
-
-	//ポリモーフィズムを使ってインスタンスを消す
-	for (int i = 0; i < 2; i++) {
-		delete weapon[i];
-		//消した後にnullptr入れる
-		weapon[i] = nullptr;
-	}
-
+	
 	return 0;
 }
 
