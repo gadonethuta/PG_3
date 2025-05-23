92da7254c2702ee7f1dec3216876a2d037b98e7c
gadonethuta <k024g1035@g.neec.ac.jp>
2025-05-23 14:21:13 +0900

    finish

--- PG_3/main.cpp
+++ PG_3/main.cpp
@@ -3,15 +3,32 @@
 #include <time.h>
 #include <Windows.h>
 
-//敵クラス入れる
-#include "Enemy.h"
+template<typename T>
+class Mathing {
+public:
+	T num1;
+	T num2;
+
+	Mathing(T _num1, T _num2) : num1(_num1), num2(_num2) {}
+
+	T Min() {
+		if (num1 < num2) {
+			return num1;
+		}
+		return num2;
+	}
+};
 
 
 int main() {
-	//敵のインスタンスを作る
-	Enemy *enemy_ = new Enemy();
-	//敵の処理
-	enemy_->Update();
+	Mathing<int>m1(10, 2);
+	Mathing<float>m2(11.3f, 15.2f);
+	Mathing<double>m3(2.0, 12314.2);
+
+	printf("min(%d, %d) = %d\n", m1.num1, m1.num2, m1.Min());
+	printf("min(%f, %f) = %f\n", m2.num1, m2.num2, m2.Min());
+	printf("min(%lf, %lf) = %lf\n", m3.num1, m3.num2, m3.Min());
+
 	return 0;
 }
 
