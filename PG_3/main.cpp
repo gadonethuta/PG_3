a7b13af9d3b105bf18cd1b52f8f1f69296d3d7a8
gadonethuta <k024g1035@g.neec.ac.jp>
2025-06-24 10:18:28 +0900

    reset

--- PG_3/main.cpp
+++ PG_3/main.cpp
@@ -3,26 +3,7 @@
 #include <time.h>
 #include <Windows.h>
 
-#include "Circle.h"
-#include "Rect.h"
-
 int main() {
-	//IShape2つ作る
-	IShape *shape[2];
-	shape[0] = new Circle();
-	shape[1] = new Rect();
-
-	//ポリモーフィズムで関数を呼ぶ
-	for (int i = 0; i < 2; i++) {
-		shape[i]->Size();
-		shape[i]->Draw();
-	}
-
-	//ポリモーフィズムでインスタンスを消す
-	for (int i = 0; i < 2; i++) {
-		delete shape[i];
-		shape[i] = nullptr;
-	}
 
 	return 0;
 }
